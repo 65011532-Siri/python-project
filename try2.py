d9436672260b859e51abcaf824b0ce2845944357
65011532-Siri <65011532@kmitl.ac.th>
2022-12-12 22:18:48 +0700

    6

--- try2.py
+++ try2.py
@@ -1,4 +1,16 @@
-import numpy as np
-b = np.array([[6,5,2],[7,1,3],[8,0,9]])
-print(b)
-print(np.sum(b, axis=0)[0])
+food = 0
+trans = 1
+enter = 0
+sport = 1
+invest = 0
+other = 1
+expense_sum = [food,trans,enter,sport,invest,other]
+expense_cate = ["Food and Beverage", "Transportatino and gas", "Entertainment","Sport","Investment","Others Expenses"]
+exp = [0.1,0,0.1,0.1,0.1,0]
+if food == 0:
+    expense_sum.remove(food)
+    expense_cate.remove("Food and Beverage")
+    exp.remove(0.1)
+print(expense_sum)
+print(expense_cate)
+print(exp)
